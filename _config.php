83f29f9c9a90c6855f0c2f50048a8816b691441f
sheadawson <shea@silverstripe.com.au>
2012-03-30 22:33:58 +1100

    add extension to controller in _config

--- _config.php
+++ _config.php
@@ -5,6 +5,8 @@ if($dir != "zenmessage") {
 	user_error('Directory name must be "zenmessage" (currently "'.$dir.'")',E_USER_ERROR);
 }
 
+Object::add_extension('Controller', 'ZenMessageExtension');
+
 // Add this line to your site if you want to use the default css
 
 //Requirements::css('zenmessage/css/zenmessage.css');
