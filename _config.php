7099a2c6e6f857cbe748ab70d23c8b1fba986446
Shea <shea@livesource.co.nz>
2016-05-09 16:36:10 +1200

    update to use yml config

--- _config.php
+++ _config.php
@@ -4,9 +4,3 @@ $dir = basename(dirname(__FILE__));
 if($dir != "zenmessage") {
 	user_error('Directory name must be "zenmessage" (currently "'.$dir.'")',E_USER_ERROR);
 }
-
-Object::add_extension('Controller', 'ZenMessageExtension');
-
-// Add this line to your site if you want to use the default css
-
-//Requirements::css('zenmessage/css/zenmessage.css');
